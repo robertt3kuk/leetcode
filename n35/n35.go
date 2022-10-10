154b70aab9d7d195949e8e92ff00ff07812479de
biqontie <awesome.abaildaev@yandex.kz>
2022-10-10 21:26:19 +0600

    n35 done

---
+++ n35/n35.go
@@ -0,0 +1,22 @@
+package n35
+
+func searchInsert(nums []int, target int) int {
+	high := len(nums) - 1
+	low := 0
+	mid := high + low/2
+	for {
+		if high <= low {
+			break
+		}
+		if target == nums[mid] {
+			return mid
+		}
+		mid = high + low/2
+		if target > nums[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return high + 1
+}
